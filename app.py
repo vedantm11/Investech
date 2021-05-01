77d634a7bb6dc7cfa6d7c7c5d65b6d1bbcfc839c
Archit <archit.hadge@gmail.com>
2021-05-01 15:44:55 +0530

    Load/Filter data function

--- app.py
+++ app.py
@@ -27,3 +27,18 @@ def filter_company_data(df_company, esg_categories, start, end):
     #                         (df_company.DATE <= end)]
     df_company = df_company[df_company.DATE.between(start, end)]
     return df_company
+
+@st.cache(show_spinner=False,suppress_st_warning=True)
+def filter_publisher(df_company,publisher):
+    if publisher != 'all':
+        df_company = df_company[df_company['SourceCommonName'] == publisher]
+    return df_company
+
+@st.cache(show_spinner=False, suppress_st_warning=True,allow_output_mutation=True)
+def load_data(start_data, end_data):
+    data = Data().read(start_data, end_data)
+    companies = data["data"].Organization.sort_values().unique().tolist()
+    companies.insert(0,"Select a Company")
+    return data, companies
+
+
